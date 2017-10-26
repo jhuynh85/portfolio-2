40b47703fb33d35e4957f0afb3ff3167d5cda5d3
Joseph Huynh <joe.huynh@gmail.com>
2017-10-26 00:53:16 -0700

    Got CSS modules working

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CSSModules from "react-css-modules";
+import styles from "./First.module.css";
 
 const Main = () => (
   <div>
-    <h1>Testing</h1>
+    <h1 styleName="header">Testing</h1>
   </div>
-)
+);
 
-export default Main
+export default CSSModules(Main, styles);
