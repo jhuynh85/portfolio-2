d9b3eec67174e7c6f9b80442ef30a4defc4c4f38
Joseph Huynh <joe.huynh@gmail.com>
2017-11-06 21:55:11 -0800

    Changed to fixed header

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,12 +3,12 @@ import styles from "./Footer.module.css";
 import CSSModules from "react-css-modules";
 
 // Get current year
-const date = new Date();
-const year = date.getFullYear();
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
 
 const Footer = () => (
-  <footer className="page-footer blue darken-1" styleName="centeredText">
-    Copyright © {year} Joseph Huynh
+  <footer className="page-footer" styleName="centeredText">
+    Copyright © {currentYear} Joseph Huynh
   </footer>
 );
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,36 @@
 import React from "react";
 import CSSModules from "react-css-modules";
 import styles from "./Header.module.css";
-import { Link } from "react-scroll";
+import { Link, animateScroll as Scroller } from "react-scroll";
 
 class Header extends React.Component {
+  scrollToTop = () => {
+    Scroller.scrollToTop();
+  }
 
   render () {
     return(
-      <nav>
-        <div className="nav-wrapper blue darken-1" styleName="nav-wrapper">
-          <a href="#!" styleName="logo">Joseph Huynh</a>
-          <a href="#" data-activates="burger-menu" className="button-collapse"><i className="material-icons">menu</i></a>
-          <ul className="right hide-on-med-and-down">
-            <li><a>About</a></li>
-            <li><Link to="portfolio" smooth={true}>Portfolio</Link></li>
-            <li><Link to="contact" smooth={true}>Contact</Link></li>
-          </ul>
-          <ul className="side-nav" id="burger-menu">
-            <li><a>About</a></li>
-            <li><Link to="portfolio" smooth={true}>Portfolio</Link></li>
-            <li><Link to="contact" smooth={true}>Contact</Link></li>
-          </ul>
+      <div>
+        <div className="navbar-fixed">
+          <nav>
+            <div className="nav-wrapper blue darken-1" styleName="nav-wrapper">
+              <a href="#!" styleName="logo">Joseph Huynh</a>
+              <a href="#" data-activates="burger-menu" className="button-collapse"><i className="material-icons">menu</i></a>
+              <ul className="right hide-on-med-and-down">
+                <li><a onClick={this.scrollToTop}>About</a></li>
+                <li><Link to="portfolio" smooth={true}>Portfolio</Link></li>
+                <li><Link to="contact" smooth={true}>Contact</Link></li>
+              </ul>
+            </div>
+          </nav>
         </div>
-      </nav>);
+        <ul className="side-nav" id="burger-menu">
+          <li><a onClick={this.scrollToTop}>About</a></li>
+          <li><Link to="portfolio" smooth={true}>Portfolio</Link></li>
+          <li><Link to="contact" smooth={true}>Contact</Link></li>
+        </ul>
+      </div>
+    );
   }
 }
 
